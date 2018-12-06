e33024a4c15e21b50e1081a3fe322c2aeff5fa8e
zwazoo <liudmilla.kharchenko@gmail.com>
2018-12-06 16:38:42 +0200

    init

---
+++ js/widget.js
@@ -0,0 +1,37 @@
+;
+(function () {
+    "use strict";
+
+    const xhr = new XMLHttpRequest();
+
+    const employeesList = document.querySelector('.bulleted'),
+        employeeTmpl = document.querySelector('#li-tmpl').innerHTML;
+
+    let employeeListHTML;
+
+    xhr.open('GET', "../data/employees.json");
+
+    xhr.send();
+
+    xhr.onload = function () {
+        let ajax = this;
+        const data = JSON.parse(ajax.response);
+
+        data.forEach(function (employees) {
+            let status;
+            if(employees.inoffice==true) {
+                status="in";
+            }else{
+                status="out";
+            }
+            
+            employeeListHTML += employeeTmpl
+            .replace(/{{inoffice}}/ig, status)
+            .replace(/{{name}}/ig, employees.name)
+        });
+
+        employeesList.innerHTML = employeeListHTML;
+
+    };
+
+})();
